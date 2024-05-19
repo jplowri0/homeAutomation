ce0b3de55d845566acdc478e42d2e76534c8ed98
jplow <jplow.tech@gmail.com>
2024-05-19 10:56:32 +1000

    switch messaging done

---
+++ switch_bot_alert_all_away_doors_off.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Define the file path
+CSV_FILE="switch_bot_alert_all_away_doors_state.csv"
+
+# Get the current date and time
+CURRENT_DATE=$(date '+%Y-%m-%d')
+CURRENT_TIME=$(date '+%H:%M:%S')
+
+# Define the door state (0 for closed)
+DOOR_STATE=0
+
+# Check if the CSV file exists and is not empty
+if [ ! -s "$CSV_FILE" ]; then
+	# If the file does not exist or is empty, write the header
+	echo "Date,Time,State" >"$CSV_FILE"
+fi
+
+# Append the current date, time, and door state to the CSV file
+echo "$CURRENT_DATE,$CURRENT_TIME,$DOOR_STATE" >>"$CSV_FILE"
+
+python3 bot_message_switch_off.py
