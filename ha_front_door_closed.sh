8d0e3e2f3fcc159aae7b6f805ce4234f3c7cec54
John Plowright <johnplowright@192-168-1-124.tpgi.com.au>
2024-09-08 16:04:04 +1000

    front door updated for ha

---
+++ ha_front_door_closed.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Define the file path
+CSV_FILE="log_front_door.csv"
+
+# Get the current date and time
+CURRENT_DATE=$(date '+%Y-%m-%d')
+CURRENT_TIME=$(date '+%H:%M:%S')
+
+# Define the door state (0 for closed)
+DOOR_STATE=0
+
+# Check if the CSV file exists and is not empty
+if [ ! -s "$CSV_FILE" ]; then
+	# If the file does not exist or is empty, write the header
+	echo "Date,Time,State" >"$CSV_FILE"
+fi
+
+# Append the current date, time, and door state to the CSV file
+echo "$CURRENT_DATE,$CURRENT_TIME,$DOOR_STATE" >>"$CSV_FILE"
+
+mv log_front_door.csv /config/data/
