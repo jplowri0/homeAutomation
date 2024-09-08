a16489e3a2704e8e2208a4ee6fc0accc0b3a945e
root <root@sensors.swest.com>
2024-09-08 12:11:14 +0000

    office Door

---
+++ office_door_open.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Define the file path
+CSV_FILE="log_office_door.csv"
+
+# Get the current date and time
+CURRENT_DATE=$(date '+%Y-%m-%d')
+CURRENT_TIME=$(date '+%H:%M:%S')
+
+# Define the door state (0 for closed)
+DOOR_STATE="Open"
+
+# Check if the CSV file exists and is not empty
+if [ ! -s "$CSV_FILE" ]; then
+	# If the file does not exist or is empty, write the header
+	echo "Date,Time,State" >"$CSV_FILE"
+fi
+
+# Append the current date, time, and door state to the CSV file
+echo "$CURRENT_DATE,$CURRENT_TIME,$DOOR_STATE" >>"$CSV_FILE"
